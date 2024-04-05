e916a9a26e5de1e3900e5ffeb0a207db1bdd0d77
Мария <mashunia0306@yandex.ru>
2024-04-05 19:03:56 +0300

    add: сервис статистики

---
+++ stats/stats-svc/src/main/java/ru/practicum/statsvc/model/Event.java
@@ -0,0 +1,27 @@
+package ru.practicum.statsvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "statistics")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 128)
+    private String app;
+    @Column(length = 128)
+    private String uri;
+    @Column(length = 40)
+    private String ip;
+    private LocalDateTime timestamp;
+}
